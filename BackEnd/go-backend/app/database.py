f077429ae9f36d5c735c82d1caa4fef8d90c7358
Ritik Sharma <ritiksharma.14y@gmail.com>
2025-11-17 19:10:11 +0530

    Refactor MongoDB connection to use MONGO variable
    Updated MongoDB connection string to use environment variable.

--- BackEnd/go-backend/app/database.py
+++ BackEnd/go-backend/app/database.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb+srv://ritiksharma14y_db_user:blKgkzCQQXhuCBNf@ritik.ta2zrff.mongodb.net/studymate")
+client = MongoClient(MONGO)
 db = client["studymate"]
 users_collection = db["users"]
 posts_collection = db["posts"]
+
