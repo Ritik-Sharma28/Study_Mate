14dfa6eeccfd14dd8f2588d9c254d80c3fe39033
Ritik Sharma <ritiksharma.14@gmail.com>
2025-12-20 16:32:13 +0530

    final

--- FrontEnd/src/services/apiService.js
+++ FrontEnd/src/services/apiService.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 
 const apiClient = axios.create({
-  baseURL:  import.meta.env.VITE_API_KEY,
+  baseURL:  import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
 });
 
